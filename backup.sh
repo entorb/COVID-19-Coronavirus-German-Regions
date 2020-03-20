7ae78cbee506f42d6afb138ffcd9eb4501c5afbc
Torben <59419684+entorb@users.noreply.github.com>
2020-03-21 00:42:42 +0100

    backup script

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 # initialdir=$PWD
 DATE=`date +"%Y-%m-%d_%H-%M"`
 
-rm index.html
-wget -q https://entorb.net/COVID-19-coronavirus/index.html
+rm index-online.html
+wget -q -O index-online.html https://entorb.net/COVID-19-coronavirus/index.html 
 zip -9 backup/covid-$DATE.zip ./* scripts-gnuplot/* data/* 
