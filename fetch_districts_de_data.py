2ce9ab9bb903b6ad84efd1aed59c3bcccd236bdc
Peter Schwede <pschwede@posteo.de>
2020-03-22 11:10:45 +0100

    script for fetching data of German districts

--- fetch_districts_de_data.py
+++ fetch_districts_de_data.py
@@ -6,11 +6,16 @@ German regions in more detail.
 Data: the ArcGIS REST API of the RKI Corona Dashboard
 """
 
-from typing import Dict, List
+from typing import Dict
+import os
+import subprocess
+import datetime
 import logging
+
 import requests
 
 
+LOG = logging.getLogger(__name__)
 SEP = "\t"
 TMPDIR = "./tmp/"
 API_STRING = ("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/"
@@ -20,37 +25,65 @@ API_STRING = ("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/service
               "&resultRecordCount=1000&cacheHint=true")
 
 
-def dict_to_table(json: Dict):
+class NoResponseException(Exception):
+    """Exception class for data downloads."""
+
+
+def dict_to_table(json: Dict, add_header: bool):
     """
     Prints the json result from the API as separated values.
     """
     for row, obj in enumerate(json['features']):
-        if row == 0:
+        if add_header and row == 0:
             line = ""
             for col, item in enumerate(obj["attributes"].items()):
-                line += "%s%s" % (SEP if col else "", item[0])
-            yield line
+                line += (SEP if col else "") + item[0]
+            yield line + SEP + "Request date"
         line = ""
         for col, item in enumerate(obj["attributes"].items()):
-            line += "%s%s" % (SEP if col else "", item[1])
-        yield line
+            line += (SEP if col else "") + str(item[1])
+        yield line + SEP + datetime.date.isoformat(datetime.date.today())
 
 
-def main(args: List[str]) -> int:
+def fetch_data(url: str) -> Dict:
     """
-    No arguments required.
+    Download json file from given api_string.
     """
-    response = requests.get(API_STRING)
+    response = requests.get(url)
 
     if response.status_code != 200:
-        logging.error("No response.")
-        return 1
+        raise NoResponseException("No response, status code: %d" % response.status_code)
 
-    for line in dict_to_table(response.json()):
-        print(line)
-    return 0
+    return response.json()
+
+
+def main(args: object) -> int:
+    """
+    First argument: file for storage that will also be committed.
+    """
+    try:
+        with open(args.filename, 'a') as outfile:
+            for line in dict_to_table(fetch_data(args.url),
+                                      add_header=os.stat(args.filename).st_size == 0):
+                outfile.write("%s\n" % line)
+    except NoResponseException as exc:
+        LOG.error(exc)
+        return 1
+    except subprocess.CalledProcessError as exc:
+        LOG.error(exc)
+        return exc.returncode
+    else:
+        return 0
 
 
 if __name__ == "__main__":
     import sys
-    sys.exit(main(sys.argv))
+    import argparse
+
+    PARSER = argparse.ArgumentParser(description="Fetch and commit current data.")
+    PARSER.add_argument('filename', type=str,
+                        help="File to store and commit")
+    PARSER.add_argument('-u', '--url', type=str, default=API_STRING,
+                        help="REST URL")
+
+    sys.exit(main(PARSER.parse_args()))
