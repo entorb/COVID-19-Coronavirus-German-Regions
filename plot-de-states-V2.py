8cb95125da449fedb07fe9b2909a016c72c86bd5
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-07-24 10:56:36 +0200

    plot states python V2

--- plot-de-states-V2.py
+++ plot-de-states-V2.py
@@ -1,143 +1,171 @@
 # plot via pandas and matplotlib
+# from matplotlib.colors import LogNorm
 import glob
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import locale
+# import numpy as np
 
-# my helper modules
-import helper
+import helper  # my helper modules
+
+
+# plt.style.use('ggplot')
 
 # DE date format: Okt instead of Oct
 locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
 
+
+def getNameFromCode(code: str) -> str:
+    if code == 'BW':
+        long_name = "Baden-Württemberg"
+    elif code == 'BY':
+        long_name = "Bayern"
+    elif code == 'BE':
+        long_name = "Berlin"
+    elif code == 'BB':
+        long_name = "Brandenburg"
+    elif code == 'HB':
+        long_name = "Bremen"
+    elif code == 'HH':
+        long_name = "Hamburg"
+    elif code == 'HE':
+        long_name = "Hessen"
+    elif code == 'MV':
+        long_name = "Mecklenburg-Vorpommern"
+    elif code == 'NI':
+        long_name = "Niedersachsen"
+    elif code == 'NW':
+        long_name = "Nordrhein-Westfalen"
+    elif code == 'RP':
+        long_name = "Rheinland-Pfalz"
+    elif code == 'SL':
+        long_name = "Saarland"
+    elif code == 'SN':
+        long_name = "Sachsen"
+    elif code == 'ST':
+        long_name = "Sachsen-Anhalt"
+    elif code == 'SH':
+        long_name = "Schleswig-Holstein"
+    elif code == 'TH':
+        long_name = "Thüringen"
+    elif code == 'DE-total':
+        long_name = "Deutschland"
+    return long_name
+
+
 for datafile in glob.glob("data/de-states/de-state-*.tsv"):
+    # for datafile in ("data/de-states/de-state-DE-total.tsv",):
     (filepath, fileName) = os.path.split(datafile)
     (fileBaseName, fileExtension) = os.path.splitext(fileName)
     code = fileBaseName[9:]
-    # if code == "DE-total":
-    #     code = "DE"
+    long_name = getNameFromCode(code)
 
-    if code == 'BW' : long_name = "Baden-Württemberg"
-    elif code == 'BY' : long_name = "Bayern"
-    elif code == 'BE' : long_name = "Berlin"
-    elif code == 'BB' : long_name = "Brandenburg" 
-    elif code == 'HB' : long_name = "Bremen"
-    elif code == 'HH' : long_name = "Hamburg"
-    elif code == 'HE' : long_name = "Hessen"
-    elif code == 'MV' : long_name = "Mecklenburg-Vorpommern"
-    elif code == 'NI' : long_name = "Niedersachsen"
-    elif code == 'NW' : long_name = "Nordrhein-Westfalen"
-    elif code == 'RP' : long_name = "Rheinland-Pfalz"
-    elif code == 'SL' : long_name = "Saarland"
-    elif code == 'SN' : long_name = "Sachsen"
-    elif code == 'ST' : long_name = "Sachsen-Anhalt"
-    elif code == 'SH' : long_name = "Schleswig-Holstein"
-    elif code == 'TH' : long_name = "Thüringen"
-    elif code == 'DE-total': long_name = "Deutschland"
-
-
-
-
-
-
-
-    # print(code)
+    #
+    # Read and setup data
+    #
     df = pd.read_csv(datafile, sep="\t")
     df = df[["Date", "Cases_Last_Week_Per_100000",
-             "Cases_Last_Week_7Day_Percent","Deaths_Last_Week_Per_Million","DIVI_Intensivstationen_Covid_Prozent"]]
+             "Cases_Last_Week_7Day_Percent", "Deaths_Last_Week_Per_Million", "DIVI_Intensivstationen_Covid_Prozent"]]
     # use date/current as index
     df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
     df.set_index(['Date'], inplace=True)
 
-    df = df.rename(columns={"Cases_Last_Week_Per_100000": "Inzidenz", "Cases_Last_Week_7Day_Percent": "Inzidenzanstieg", "Deaths_Last_Week_Per_Million": "Tote","DIVI_Intensivstationen_Covid_Prozent":"Intensivstationsbelegung"}, errors="raise")
+    # nicer names for the data colums
+    df = df.rename(columns={"Cases_Last_Week_Per_100000": "Inzidenz", "Cases_Last_Week_7Day_Percent": "Inzidenzanstieg",
+                   "Deaths_Last_Week_Per_Million": "Tote", "DIVI_Intensivstationen_Covid_Prozent": "Intensivstationsbelegung"}, errors="raise")
 
-    # negative -> 0
+    # negative values -> 0
     df[df < 0] = 0
 
+    #
+    # initialize plot
+    #
+    fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(6, 8)  # default = 6.4,4.8
+                             , dpi=100
+                             )
 
-    # Plot 1: y1: Inzidenz, y2: Cases_Last_Week_7Day_Percent
-    plt.suptitle(f"Inzidenzwert und -anstieg in {long_name}") # super title
-    plt.title("by Torben https://entorb.net", fontsize=8) 
+    fig.suptitle(f"COVID-19 in {long_name}")  # super title
+    axes[0].set_title("Inzidenzwert und -anstieg", fontsize=10)
+    axes[1].set_title("Tote und Intensivstationsbelegung", fontsize=10)
 
-    ax1 = df.Inzidenz.plot(
-        color="blue", legend=False, secondary_y=False, zorder=2)
-    ax1.set_zorder(2)
-    # important: transparent background for line plot
-    ax1.set_facecolor('none')
-    ax2 = df.Inzidenzanstieg.plot.area(color="red",
-                                                    legend=False, secondary_y=True, zorder=1)
-    ax2.set_zorder(1)
+    # define colors for data
+    colors = (('blue', 'red'), ('purple', 'green'))
 
-    ax1.set_ylim(0, )
-    ax2.set_ylim(0, 200)
+    #
+    # plot the data
+    #
+    df.Inzidenz.plot(ax=axes[0], color=colors[0][0], legend=False,
+                     secondary_y=False, zorder=2, linewidth=2.0)
+    df.Inzidenzanstieg.plot.area(
+        ax=axes[0], color=colors[0][1], legend=False, secondary_y=True, zorder=1)
+    df.Tote.plot(ax=axes[1], color=colors[1][0], legend=False,
+                 secondary_y=False, zorder=2, linewidth=2.0)
+    df.Intensivstationsbelegung.plot.area(
+        ax=axes[1], color=colors[1][1], legend=False, secondary_y=True, zorder=1, linewidth=2.0)
 
-    # plt.xlabel("")
-    ax1.set_xlabel("")
-    ax2.set_xlabel("")
+    #
+    # Axis layout, text and range
+    #
 
-    ax1.set_ylabel('Inzidenz (7 Tage)')
+    # axes[0].autoscale()
+    axes[0].set_ylim(0, )
+    axes[0].right_ax.set_ylim(0, 200)
+    axes[1].set_ylim(0, )
+    axes[1].right_ax.set_ylim(0, 50)
 
-    ax1.yaxis.label.set_color('blue')
-    ax1.tick_params(axis='y', colors='blue')
+    axes[0].set_ylabel('Inzidenz (7 Tage)')
+    axes[0].yaxis.label.set_color(colors[0][0])
+    axes[0].tick_params(axis='y', colors=colors[0][0])
+    axes[1].set_ylabel('Tote (7 Tage) pro Millionen')
+    axes[1].yaxis.label.set_color(colors[1][0])
+    axes[1].tick_params(axis='y', colors=colors[1][0])
+    axes[1].grid(zorder=0)
 
-    ax2.set_ylabel('Anstieg (7 Tage)')
-    ax2.yaxis.label.set_color('red')
-    ax2.tick_params(axis='y', colors='red')
-    ax2.yaxis.set_major_formatter(mtick.PercentFormatter())
+    axes[0].right_ax.set_ylabel(
+        'Inzidenzanstieg (7 Tage)')
+    axes[0].right_ax.yaxis.label.set_color(colors[0][1])
+    axes[0].right_ax.tick_params(axis='y', colors=colors[0][1])
+    axes[0].right_ax.yaxis.set_major_formatter(mtick.PercentFormatter())
 
+    axes[1].right_ax.set_ylabel(
+        'Intensivstationen Anteil COVID-Patienten')
+    axes[1].right_ax.yaxis.label.set_color(colors[1][1])
+    axes[1].right_ax.tick_params(axis='y', colors=colors[1][1])
+    axes[1].right_ax.yaxis.set_major_formatter(mtick.PercentFormatter())
 
-    # plt.grid()
-    ax1.grid()
-    # ax2.grid()
-    plt.tight_layout()
+    axes[1].set_xlabel("")
+
+    # zorder problem
+    # 1. per axis
+    # 2. per series in axis including grid
+    # Problem: can't solve the problem, that data of the seconday y axis is plotted below the grid of the 1st axis
+    axes[0].grid(zorder=-1)
+    axes[1].grid(zorder=-1)
+
+    axes[0].set_zorder(axes[0].right_ax.get_zorder()+1)
+    axes[0].patch.set_visible(False)
+    axes[1].set_zorder(axes[1].right_ax.get_zorder()+1)
+    axes[1].patch.set_visible(False)
+
+    # print(axes[0].get_zorder(), axes[0].right_ax.get_zorder())
+    # axes[0].grid(zorder=0)
+    # axes[0].set_zorder(2)
+    # axes[0].right_ax.set_zorder(1)
+    # axes[0].set_facecolor('none')
+    # # axes[1].grid(zorder=0)
+    # axes[1].set_zorder(2)
+    # axes[1].right_ax.set_zorder(1)
+    # axes[1].set_facecolor('none')
+
+    # add text to bottom right
+    plt.gcf().text(1.0, 0.0, s="by Torben https://entorb.net , based on RKI and DIVI data", fontsize=8,
+                   horizontalalignment='right', verticalalignment='bottom', rotation='vertical')
+
+    fig.tight_layout()
+    # plt.show()
 
     plt.savefig(
         fname=f"plots-python/de-states/{fileBaseName}.png", format='png')
     plt.close()
-
-
-    # # Plot 2: y1: Inzidenz und , y2: Cases_Last_Week_7Day_Percent
-    # plt.suptitle(f"{long_name}") # super title
-    # plt.title("by Torben https://entorb.net", fontsize=8) 
-
-    # ax1 = df.Inzidenz.plot(
-    #     color="blue", legend=False, secondary_y=False, zorder=1)
-    # df.Tote.plot(ax=ax1,color="black", legend=False, secondary_y=False, zorder=2)
-    # ax1.set_ylabel("Wochen-Inzidenz und -Tote pro 1.000.000")
-    # # ax2 = ax1.twinx()
-    # ax2 = df.Intensivstationsbelegung.plot(# ax=ax2,
-    #     color="red", legend=False, secondary_y=True, zorder=3)
-    # ax2.set_ylabel("Intensivstationsbelegung durch COVID-19")
-
-    # # fig, ax = plt.subplots()  #create figure and axes
-    # # ax1 = ax[0]
-
-    # # plt.xlabel("")
-    # ax1.set_xlabel("")
-    # ax2.set_xlabel("")    
-
-    # # plt.tick_params(direction="in")
-    # # ax1.tick_params(direction="in")
-    # # ax2.tick_params(direction="in")
-
-
-    # ax2.yaxis.set_major_formatter(mtick.PercentFormatter())
-    # ax2.yaxis.label.set_color('red')
-    # ax2.tick_params(axis='y', colors='red')
-    # # ax2.legend(['Intensivstationsbelegung'],loc='best')
-
-    # # plt.legend(["Inzidenz", "Tote", "Intensivstationsbelegung"], loc='best')
-
-    # ax1.set_ylim(0, )
-    # ax2.set_ylim(0, )
-
-    # # plt.grid()
-    # ax1.grid()
-    # # ax2.grid()
-    # plt.tight_layout()
-    # plt.show()
-    # break
-
-
