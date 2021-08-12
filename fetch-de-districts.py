d385f5843195ea7e0359bbdbdc85cf976d3aea02
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-08-12 11:55:35 +0200

    fix

--- fetch-de-districts.py
+++ fetch-de-districts.py
@@ -560,38 +560,38 @@ def export_latest_data(d_districts_data: dict):
             #         d2['Deaths_Per_Million'], 0)
 
 
-def count_zero_cases_last_week(d_districts_data):
-    # calc number of districts with Cases_Last_Week == 0
-    d_count_districts_with_zero_cases_last_week_per_date = {}
-    d_count_districts_with_50_cases_last_week_per_date = {}
-    for lk_id, l_time_series in d_districts_data.items():
-        for d in l_time_series:
-            date = d["Date"]
-            if date not in d_count_districts_with_zero_cases_last_week_per_date:
-                d_count_districts_with_zero_cases_last_week_per_date[date] = 0
-            if date not in d_count_districts_with_50_cases_last_week_per_date:
-                d_count_districts_with_50_cases_last_week_per_date[date] = 0
-            if d["Cases_Last_Week_Per_100000"] != 0:
-                d_count_districts_with_zero_cases_last_week_per_date[date] += 1
-            if d["Cases_Last_Week_Per_100000"] >= 50:
-                d_count_districts_with_50_cases_last_week_per_date[date] += 1
+# def count_zero_cases_last_week(d_districts_data):
+#     # calc number of districts with Cases_Last_Week == 0
+#     d_count_districts_with_zero_cases_last_week_per_date = {}
+#     d_count_districts_with_50_cases_last_week_per_date = {}
+#     for lk_id, l_time_series in d_districts_data.items():
+#         for d in l_time_series:
+#             date = d["Date"]
+#             if date not in d_count_districts_with_zero_cases_last_week_per_date:
+#                 d_count_districts_with_zero_cases_last_week_per_date[date] = 0
+#             if date not in d_count_districts_with_50_cases_last_week_per_date:
+#                 d_count_districts_with_50_cases_last_week_per_date[date] = 0
+#             if d["Cases_Last_Week_Per_100000"] != 0:
+#                 d_count_districts_with_zero_cases_last_week_per_date[date] += 1
+#             if d["Cases_Last_Week_Per_100000"] >= 50:
+#                 d_count_districts_with_50_cases_last_week_per_date[date] += 1
 
-    # Export as CSV
-    with open('data/de-districts/de-districts-zero_cases_last_week.tsv', mode='w', encoding='utf-8', newline='\n') as fh_csv:
-        csvwriter = csv.writer(fh_csv, delimiter='\t')
-        csvwriter.writerow(
-            ("Date", "Landkreise mit Neu-Infektionen in 7 Tagen"))
-        for date in sorted(d_count_districts_with_zero_cases_last_week_per_date.keys()):
-            csvwriter.writerow(
-                (date, d_count_districts_with_zero_cases_last_week_per_date[date]))
-    # Export as CSV
-    with open('data/de-districts/de-districts-50_cases_last_week.tsv', mode='w', encoding='utf-8', newline='\n') as fh_csv:
-        csvwriter = csv.writer(fh_csv, delimiter='\t')
-        csvwriter.writerow(
-            ("Date", "Landkreise mit Inzidenz (Cases_Last_Week_Per_100000) > 50"))
-        for date in sorted(d_count_districts_with_50_cases_last_week_per_date.keys()):
-            csvwriter.writerow(
-                (date, d_count_districts_with_50_cases_last_week_per_date[date]))
+#     # Export as CSV
+#     with open('data/de-districts/de-districts-zero_cases_last_week.tsv', mode='w', encoding='utf-8', newline='\n') as fh_csv:
+#         csvwriter = csv.writer(fh_csv, delimiter='\t')
+#         csvwriter.writerow(
+#             ("Date", "Landkreise mit Neu-Infektionen in 7 Tagen"))
+#         for date in sorted(d_count_districts_with_zero_cases_last_week_per_date.keys()):
+#             csvwriter.writerow(
+#                 (date, d_count_districts_with_zero_cases_last_week_per_date[date]))
+#     # Export as CSV
+#     with open('data/de-districts/de-districts-50_cases_last_week.tsv', mode='w', encoding='utf-8', newline='\n') as fh_csv:
+#         csvwriter = csv.writer(fh_csv, delimiter='\t')
+#         csvwriter.writerow(
+#             ("Date", "Landkreise mit Inzidenz (Cases_Last_Week_Per_100000) > 50"))
+#         for date in sorted(d_count_districts_with_50_cases_last_week_per_date.keys()):
+#             csvwriter.writerow(
+#                 (date, d_count_districts_with_50_cases_last_week_per_date[date]))
 
 
 d_ref_landkreise = fetch_and_prepare_ref_landkreise()
@@ -602,6 +602,6 @@ d_districts_data = download_all_data()
 
 d_districts_data = join_with_divi_data(d_districts_data)
 
-count_zero_cases_last_week(d_districts_data)
-export_data(d_districts_data)
+# count_zero_cases_last_week(d_districts_data)
 export_latest_data(d_districts_data)
+export_data(d_districts_data)
