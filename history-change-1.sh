ac5c77f92054650bc4448664668ed957ad458fc6
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-08-15 11:42:50 +0200

    history change research

---
+++ history-change-1.sh
@@ -0,0 +1,54 @@
+#!/bun/bash
+myFilePath=data/de-districts/de-district_timeseries-09562.tsv
+# 02000 : HH
+# 09562 : ER
+
+myDateToInvestigate=2021-07-09
+
+myFileName=$(basename -- "$myFilePath")
+myFileExt="${myFileName##*.}"
+myFileName="${myFileName%.*}"
+
+
+#
+# 1. list all commits for file
+#
+
+# list all commits
+myCommitLogFile=cache/history-change.tsv
+mkdir -p cache/history-change
+git log --no-decorate --pretty=format:"%h%x09%ad%x09%an%x09%s" --date=short -- $myFilePath > $myCommitLogFile
+# --date=short : only date, not time
+# --date=iso   : include time as well
+
+# add linebreak to the end
+echo "" >> $myCommitLogFile
+
+#
+# 2. revert old versions from commits
+#
+
+# loop over all commits/changes for that file
+while read line ; do
+    set $line
+	myCommit=$1
+	myDate=$2
+    echo "$myCommit ; $myDate"
+	# revert file from commit
+	git checkout $myCommit $myFilePath
+	# copy to 
+	cp $myFilePath cache/history-change/${myFileName}_${myDate}.${myFileExt}
+done <"$myCommitLogFile"
+# rm $myCommitLogFile
+
+# revert changes to file
+# git reset -- $myFilePath
+git reset -- $myFilePath
+git checkout $myFilePath
+
+#
+# 3. extract data for specific date from old versions of file
+# TODO: do this in Python, as the column order was changed on 2021-08-13
+#
+echo -e "filedate\t"$(head -n 1 $myFilePath | cut -f1,2,3,4,5,6,7,8) > history-change.tsv
+grep -H "$myDateToInvestigate" cache/history-change/${myFileName}* | sed "s/cache\/history-change\/${myFileName}_//" | sed "s/.tsv:/\t/" | cut -f1,2,3,4,5,6,7,8,9 >> history-change.tsv
