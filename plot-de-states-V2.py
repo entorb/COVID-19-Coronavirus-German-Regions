3f49e8221f162b2da3190b22206f31cd8a6ba37c
Torben <59419684+entorb@users.noreply.github.com>
2021-08-29 05:19:18 +0200

    fine tuning and refactoring

--- plot-de-states-V2.py
+++ plot-de-states-V2.py
@@ -4,7 +4,7 @@ import glob
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
+import matplotlib.ticker as ticker
 import locale
 from math import log
 
@@ -31,6 +31,74 @@ def calc_doubling_time(percent_7day: float) -> float:
 
 
 assert calc_doubling_time(1.00) == 7
+
+
+def plot_layout(fig, axes: list, colors: list):
+    """
+    Axis layout, label text and range
+    """
+    # shared x axis
+    # remove label as date is obvious
+    axes[1].set_xlabel("")
+
+    # top plot
+    axes[0].set_title("Inzidenzwert und -anstieg", fontsize=10)
+    axes[1].set_title("Tote und Intensivstationsbelegung", fontsize=10)
+    # axis label
+    axes[0].set_ylabel('Inzidenz (7 Tage)')
+    axes[0].right_ax.set_ylabel('Inzidenzanstieg (7 Tage)')
+    axes[1].set_ylabel('Tote (7 Tage pro Millionen)')
+    axes[1].right_ax.set_ylabel('Intensivstationen Anteil COVID-Patienten')
+    # axis range
+    axes[0].set_ylim(0, )  # 0,550
+    axes[0].right_ax.set_ylim(0, 150)
+    axes[1].set_ylim(0, )  # 0,250
+    axes[1].right_ax.set_ylim(0, 40)
+    # tick freq
+    # all are set to make charts better compareable
+    axes[0].yaxis.set_major_locator(ticker.MultipleLocator(50))
+    axes[0].right_ax.yaxis.set_major_locator(ticker.MultipleLocator(25))
+    axes[1].yaxis.set_major_locator(ticker.MultipleLocator(25))
+    axes[1].right_ax.yaxis.set_major_locator(ticker.MultipleLocator(10))
+    # tick format
+    axes[0].yaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+    axes[0].right_ax.yaxis.set_major_formatter(
+        ticker.PercentFormatter(decimals=0))
+    axes[1].yaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+    axes[1].right_ax.yaxis.set_major_formatter(
+        ticker.PercentFormatter(decimals=0))
+    # color of label and ticks
+    axes[0].yaxis.label.set_color(colors[0][0])
+    axes[0].tick_params(axis='y', colors=colors[0][0])
+    axes[0].right_ax.yaxis.label.set_color(colors[0][1])
+    axes[0].right_ax.tick_params(axis='y', colors=colors[0][1])
+    axes[1].yaxis.label.set_color(colors[1][0])
+    axes[1].tick_params(axis='y', colors=colors[1][0])
+    axes[1].right_ax.yaxis.label.set_color(colors[1][1])
+    axes[1].right_ax.tick_params(axis='y', colors=colors[1][1])
+    # zorder problem
+    # 1. per axis
+    # 2. per series in axis including grid
+    # Problem: can't solve the problem, that data of the secondary y axis is plotted below the grid of the 1st axis
+    axes[0].grid(zorder=-1)
+    axes[0].set_zorder(axes[0].right_ax.get_zorder()+1)
+    axes[0].patch.set_visible(False)
+    axes[1].grid(zorder=-1)
+    axes[1].set_zorder(axes[1].right_ax.get_zorder()+1)
+    axes[1].patch.set_visible(False)
+
+    # add label text to bottom right
+    plt.gcf().text(1.0, 0.5, s="by Torben https://entorb.net , based on RKI and DIVI data", fontsize=8,
+                   horizontalalignment='right', verticalalignment='center', rotation='vertical')
+
+    # add label text to bottom right
+    plt.gcf().text(0.97, 0.5, s=("Verdopplungszeit: 25%% : %d Tage, 50%% : %d Tage, 100%% : 7 Tage" %
+                                 (round(calc_doubling_time(0.25), 0),
+                                  round(calc_doubling_time(0.5), 0))
+                                 ),
+                   fontsize=8, horizontalalignment='right', verticalalignment='center', color=colors[0][1])
+
+    fig.tight_layout()
 
 
 for datafile in glob.glob("data/de-states/de-state-*.tsv"):
@@ -73,8 +141,6 @@ for datafile in glob.glob("data/de-states/de-state-*.tsv"):
                              )
 
     fig.suptitle(f"COVID-19 in {long_name}")  # super title
-    axes[0].set_title("Inzidenzwert und -anstieg", fontsize=10)
-    axes[1].set_title("Tote und Intensivstationsbelegung", fontsize=10)
 
     # define colors for data
     colors = (('blue', 'red'), ('purple', 'green'))
@@ -91,74 +157,8 @@ for datafile in glob.glob("data/de-states/de-state-*.tsv"):
     df["Intensivstationsbelegung"].plot.area(
         ax=axes[1], color=colors[1][1], legend=False, secondary_y=True, zorder=1, linewidth=2.0)
 
-    #
-    # Axis layout, text and range
-    #
+    plot_layout(fig=fig, axes=axes, colors=colors)
 
-    axes[0].autoscale()
-    # axes[0].set_ylim(0, 550)
-    axes[0].right_ax.set_ylim(0, 150)
-    # wrong syntax
-    # axes[0].right_ax.set_ticks(range(0, 175, 25))
-    axes[1].autoscale()
-    # axes[1].set_ylim(0, 250)
-    axes[1].right_ax.set_ylim(0, 50)
-
-    axes[0].set_ylabel('Inzidenz (7 Tage)')
-    axes[0].yaxis.label.set_color(colors[0][0])
-    axes[0].tick_params(axis='y', colors=colors[0][0])
-    axes[1].set_ylabel('Tote (7 Tage pro Millionen)')
-    axes[1].yaxis.label.set_color(colors[1][0])
-    axes[1].tick_params(axis='y', colors=colors[1][0])
-#    axes[1].grid(zorder=0)
-
-    axes[0].right_ax.set_ylabel(
-        'Inzidenzanstieg (7 Tage)')
-    axes[0].right_ax.yaxis.label.set_color(colors[0][1])
-    axes[0].right_ax.tick_params(axis='y', colors=colors[0][1])
-    axes[0].right_ax.yaxis.set_major_formatter(mtick.PercentFormatter())
-
-    axes[1].right_ax.set_ylabel(
-        'Intensivstationen Anteil COVID-Patienten')
-    axes[1].right_ax.yaxis.label.set_color(colors[1][1])
-    axes[1].right_ax.tick_params(axis='y', colors=colors[1][1])
-    axes[1].right_ax.yaxis.set_major_formatter(mtick.PercentFormatter())
-
-    axes[1].set_xlabel("")
-
-    # zorder problem
-    # 1. per axis
-    # 2. per series in axis including grid
-    # Problem: can't solve the problem, that data of the seconday y axis is plotted below the grid of the 1st axis
-    axes[0].grid(zorder=-1)
-    axes[1].grid(zorder=-1)
-
-    axes[0].set_zorder(axes[0].right_ax.get_zorder()+1)
-    axes[0].patch.set_visible(False)
-    axes[1].set_zorder(axes[1].right_ax.get_zorder()+1)
-    axes[1].patch.set_visible(False)
-
-    # print(axes[0].get_zorder(), axes[0].right_ax.get_zorder())
-    # axes[0].grid(zorder=0)
-    # axes[0].set_zorder(2)
-    # axes[0].right_ax.set_zorder(1)
-    # axes[0].set_facecolor('none')
-    # # axes[1].grid(zorder=0)
-    # axes[1].set_zorder(2)
-    # axes[1].right_ax.set_zorder(1)
-    # axes[1].set_facecolor('none')
-
-    # add text to bottom right
-    plt.gcf().text(1.0, 0.5, s="by Torben https://entorb.net , based on RKI and DIVI data", fontsize=8,
-                   horizontalalignment='right', verticalalignment='center', rotation='vertical')
-
-    plt.gcf().text(0.97, 0.5, s=("Verdopplungszeit: 25%% : %d Tage, 50%% : %d Tage, 100%% : 7 Tage" %
-                                 (round(calc_doubling_time(0.25), 0),
-                                  round(calc_doubling_time(0.5), 0))
-                                 ),
-                   fontsize=8, horizontalalignment='right', verticalalignment='center', color=colors[0][1])
-
-    fig.tight_layout()
     # plt.show()
 
     plt.savefig(
